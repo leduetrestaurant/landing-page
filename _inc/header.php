e4d06e44d2ed677cb1bc572e04266fac18aaf7dc
Jord Lin <jord@kloudless.com>
2020-06-03 22:11:23 +0800

    init commit

---
+++ _inc/header.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1">
+    <meta name="description" content="" />
+    <meta name="keywords" content="" />
+    <meta property="og:site_name" content="Leduet" /> 
+    <meta property="og:title" content="Leduet" />
+    <meta property="og:description" content="">
+    <meta property="og:image" content="/ms/zh_TW/img/Ads_TW/IKEA_Business/img/ibshort2.jpg" />
+    <meta property="og:image:type" content="image/jpg" />
+    <meta property="og:image:width" content="300" />
+    <meta property="og:image:height" content="300" />
+    <meta property="og:url" content="" />
+    <meta http-equiv="Content-Type" content="text/html;charset=UTF-8" />
+    <meta http-equiv="Content-Style-Type" content="text/css" />
+    <meta http-equiv="Content-Script-Type" content="text/javascript" />
+    <meta name="robots" content="index, follow" />
+    <meta name="country" content="TW" />
+    <meta name="title" content="Leduet" />
+    <meta name="format-detection" content="telephone=no">
+
+    <title id=“docTitle”>Leduet</title>
+
+    <link rel="stylesheet" href="css/swiper.min.css" rel="stylesheet" />
+    <link rel="stylesheet" href="css/ls.css">
+    <link rel="shortcut icon" href="img/fav.png"/>
+    <link rel="bookmark" href="img/fav.png"/>
+    <link href="https://fonts.googleapis.com/css?family=Cormorant:400,500,600,600i,700|Noto+Sans+TC:100,300,400,500,700,900&display=swap&subset=chinese-traditional" rel="stylesheet">
+
+    <script src="js/swiper.min.js"></script>
+    <script src="js/jq-ib.js"></script>
+    <script src="js/main.js"></script>
+    <!-- <script src="js/TweenMax.min.js"></script> -->
+</head>
+<body>
+<div class="menuall">
+    <div class="menubtn fade-in-top22">
+        <div></div>
+        <div></div>
+        <div></div>
+    </div>
+    <a href="" class="logo">
+        <img src="img/logo-c.svg">
+    </a>
+</div>
+<header>
+    <div class="menu">
+        <a class="ani" data-id="about">ABOUT US<span>關於</span></a>
+        <a class="ani" data-id="menu">MENUS<span>菜單</span></a>
+        <a class="ani" data-id="reservate">RESERVATIONS<span>訂位</span></a>
+        <a class="ani" data-id="contact">CONTACT<span>聯絡</span></a>
+        <div class="con">
+            <div class="vol mobile" onclick="document.location='https://goo.gl/maps/mFh3YebPceACd1v8A'">
+                <p>Le Duet &emsp;NO 9, ALY 16, LN 553,</p>
+                <p>SECT 4 ZHONGXIAO EAST RD, TAIPEI</p>
+                <p>台北市忠孝東路四段553巷16弄9號</p>
+            </div>
+            <div class="vol desktop" onclick="document.location='https://goo.gl/maps/mFh3YebPceACd1v8A'">
+                <p>Le Duet</p>
+                <p>NO 9, ALY 16, LN 553, SECT 4<br>ZHONGXIAO EAST RD, TAIPEI</p>
+                <p>台北市忠孝東路四段553巷16弄9號</p>
+            </div>
+            <div class="vol">
+                <a class="linkout" href="https://www.facebook.com/leduet.tw/" target="_blank"><!-- <img class="bf" src="img/icon-fb.svg"><img class="af" src="img/icon-fb-g.svg"> --></a>
+                <a class="linkout" href="https://www.instagram.com/leduet_tw/" target="_blank"><!-- <img class="bf" src="img/icon-ig.svg"><img class="af" src="img/icon-ig-g.svg"> --></a>
+            </div>
+        </div>
+    </div>
+</header>
---
+++ index.php
@@ -0,0 +1,343 @@
+<?php 
+$pagetitle = 'Leduet - KITCHEN & BAR';
+    include '_inc/header.php';
+?>
+    <div class="cover" id="home">
+        <div class="con">
+            <img class="logo-big fade-in-top4" src="img/logo1.svg">
+            <div class="box fade-in-top66">
+                <img class="font font1" src="img/tt1.svg">
+                <div class="conb">
+                    <div>
+                        <img class="bg-photo" src="img/ho1.png">
+                    </div>
+                    <div>
+                        <img class="bg-photo" src="img/ho2.png">
+                    </div>
+                    <div>
+                        <img class="bg-photo" src="img/ho3.png">
+                    </div>
+                </div>
+                <img class="font" src="img/tt2.svg">
+            </div>
+        </div>
+        <p class="footer f1 fade-in">Copyright © Le Duet</p>
+    </div>
+    <div class="content" id="about">
+        <div class="con">
+            <h2 class="fade-in-top2">CONCEPT<span>品牌概念</span></h2>
+            <p class="blue fade-in-top3">Le Duet，法文意指雙重奏：主廚具備割烹懷石的背景，揉和法餐美學的料理手法，譜寫成一段餐桌上的質感體驗。以「時間的料理」為主題，Le Duet 將食材巧妙組合、堆疊出口感上的豐富層次感，引領出繼酸甜苦鹹外的第五種味覺「鮮味」。從文化建構觀點、以理性交織感性，品牌將物質性的食物轉化為抽象的情感連結，共感出專屬於 Le Duet 的味蕾記憶。</p>
+            <p class="english blue fade-in-top4">A duet is a musical composition for two performers in which the performers have equal importance to the piece, often a composition involving two singers or two pianists.A duet is a musical composition for two performers in which the performers have equal importance to the piece, often a composition involving two singers or two pianists.</p>
+        </div>
+        <div class="con ins sec1">
+            <h2>INSPIRATION<span>料理啟發</span></h2>
+            <div class="swiper-container1 inspirearea">
+                <div class="area swiper-wrapper">
+                    <div class="timebox swiper-slide">
+                       <img src="img/inspire1.jpg"> 
+                    </div>
+                    <div class="timebox swiper-slide">
+                       <img src="img/inspire1.jpg"> 
+                    </div>
+                    <div class="timebox swiper-slide">
+                       <img src="img/inspire1.jpg"> 
+                    </div>
+                    <div class="timebox swiper-slide">
+                      <img src="img/inspire1.jpg"> 
+                    </div>
+                    <div class="timebox swiper-slide">
+                       <img src="img/inspire1.jpg"> 
+                    </div>
+                </div>
+            </div>
+            <div class="swiper-pagination swiper-pagination1"></div>
+            <div class="fordesktop">
+                <p class="whitee">料理保留其不完美的真實狀態，並專注於整體風味的平衡展現。視覺表現屏除不必要的裝飾，以渲染、潑灑、大地感的概念妝點擺盤，象徵各種滋味香韻的軌跡。設計則是呼應料理精神，以抽象帶有流動感的油墨線條，為品牌塑造優雅、低調的靜謐氛圍，增添味覺上的的無盡想像。</p>
+                <p class="english whitee">A duet is a musical composition for two performers in which the performers have equal importance to the piece, often a composition involving two singers or two pianists. It differs from a harmony, as the performers take turns performing a solo section rather than performing simultaneously.</p>
+            </div>
+            <div class="formobile">
+                <p class="whitee">料理保留其不完美的真實狀態，並專注於整體風味的平衡展現。視覺表現屏除不必要的裝飾，以渲染、潑灑、大地感的概念妝點擺盤，象徵各種滋味香韻的軌跡。設計則是呼應料理精神，以抽象帶有流動感的油墨線條，為品牌塑造優雅、低調的靜謐氛圍，增添味覺上的的無盡想像。</p>
+                <p class="english whitee">A duet is a musical composition for two performers in which the performers have equal importance to the piece, often a composition involving two singers or two pianists. It differs from a harmony, as the performers take turns performing a solo section rather than performing simultaneously.</p>
+            </div>
+        </div>
+        <div class="con hey sec2">
+            <h2>INTERIROR<span>餐廳空間</span></h2>
+            <div class="swiper-container2 inspirearea">
+                <div class="area swiper-wrapper">
+                    <div class="timebox swiper-slide">
+                       <img src="img/interior1.jpg"> 
+                    </div>
+                    <div class="timebox swiper-slide">
+                       <img src="img/interior1.jpg"> 
+                    </div>
+                    <div class="timebox swiper-slide">
+                       <img src="img/interior1.jpg"> 
+                    </div>
+                    <div class="timebox swiper-slide">
+                      <img src="img/interior1.jpg"> 
+                    </div>
+                    <div class="timebox swiper-slide">
+                       <img src="img/interior1.jpg"> 
+                    </div>
+                </div>
+            </div>
+            <div class="swiper-pagination swiper-pagination2"></div>
+        </div>
+        <p class="footer f2">Copyright © Le Duet</p>
+    </div>
+    <div class="content menus" id="menu">
+        <div class="con">
+            <h2 class="fade-in-top2">MENU<span>菜單</span></h2>
+            <div class="boxo">
+                <p class="comingsoon fade-in-top3">Coming Soon.</p>
+            </div>
+        </div>
+        <p class="footer f3">Copyright © Le Duet</p>
+    </div>
+    <div class="content reservation" id="reservate">
+        <div class="con">
+            <h2 class="fade-in-top2">RESERVATIONS<span>訂位</span></h2>
+            <div class="bookbox fade-in-top3">
+                <p class="blue">Le Duet 接受兩個月內的線上訂位</p>
+                <p class="blue">可透過官網的訂位系統或臉書專頁的藍色按鍵</p>
+                <p class="blue">BOOK NOW 搶先預約</p><br>
+                <p class="blue">當日預約，請撥打訂位專線<a class="blue" href="tel:+886-2-37621570">02 3762 1570</a></p>
+                <p class="blue">我們將為您確認現場訂位狀況</p>
+            </div>
+            <div class="fade-in-top4">
+                <div class="bookdetail fir">
+                    <h4>一般訂位<span>General bookings</span></h4>
+                    <div class="box">
+                        <b>基本低消</b>
+                        <p>每人基本低消 NT$800</p>
+                        <p>晚間九點過後低消為每人 NT$500。</p>
+                    </div>
+                    <div class="box">
+                        <b>用餐時間</b>
+                        <p>用餐時間以「訂位時間」起始計算150分鐘</p>
+                        <p>如：訂位時間18:00，用餐時間到20:30止。</p>
+                    </div>
+                    <div class="box">
+                        <b>訂位保留</b>
+                        <p>訂位將為保留15分鐘，如逾時將視同取消訂位</p>
+                        <p>將由現場候位賓客候補座位。</p>
+                    </div>
+                    <div class="box">
+                        <b>座位安排＆人數變更</b>
+                        <p>座位將依餐廳當日訂位排定，恕無法指定座位，亦不接受現場增加人數，若有任何人數變更，請於用餐前提前與餐廳聯繫。</p>
+                    </div>
+                </div>
+                <div class="bookdetail">
+                    <h4>私人包場＆特別活動<span>Private Hire & Events</span></h4>
+                    <div class="box">
+                        <b>私人包場/ 特別活動時段</b>
+                        <p>中場 11:30 — 15:30</p>
+                        <p>晚場 17:00 — 21:00</p>
+                        <p>凌晨 21:00 — 01:00</p>
+                    </div>
+                    <div class="box">
+                        <b>客製菜單</b>
+                        <p>私人活動包場可提供特別菜單的服務。</p>
+                    </div>
+                    <div class="box">
+                        <b>預付訂金</b>
+                        <p>完成預付訂金前，訂位保留2天，如2天內未完成預付訂金，視同取消訂位。</p>
+                    </div>
+                    <div class="box">
+                        <b>改期或取消</b>
+                        <p>如需改期或取消訂位，請於用餐日一週前致電告知，以便協助相關退款動作。<br> 不接受臨時更動及取消訂位，且訂金不退還。</p>
+                    </div>
+                    <div class="box">
+                        <b>額外租金</b>
+                        <p>若有超出私人包場或特別活動時間，我們將收取額外的租金，造成不便之處敬請見諒。</p>
+                    </div>
+                </div>
+                <div class="bookdetail">
+                    <h4>慶祝特別節日<span>Celebrations & Festivals</span></h4>
+                    <div class="box">
+                        <p>如有事先告知需慶生、紀念日或其他慶祝活動， Le Duet 將招待一份盤飾甜點並上蠟燭及祝賀文字。另可訂購4、6、8吋生日蛋糕，歡迎來電洽詢。</p>
+                    </div>
+                </div>
+                <div class="bookdetail las">
+                    <h4>其他或注意事項<span>Other Notices</span></h4>
+                    <div class="box">
+                        <p>洗杯費NT$100/杯 (故不另酌收開瓶費) 。</p>
+                    </div>
+                    <div class="box">
+                        <p>所有價格以新台幣表示，並需加收一成服務費<br>（洗杯費及租金除外）。</p>
+                    </div>
+                    <div class="box">
+                        <p>為考量與顧客相關之食品安全及呈現餐點最完美的狀態，Le Duet 不提供外帶服務，亦無任何外帶的容器。請勿攜帶外食及飲料，生日蛋糕例外。</p>
+                    </div>
+                    <div class="box">
+                        <p>恕不接待12歲以下兒童。</p>
+                        <p>恕不接待寵物入內。</p>
+                        <p>無提供素食餐點。</p>
+                    </div>
+                    <div class="box">
+                        <p>菜單內容均視新鮮及季節性食材而定，Le Duet有權更換菜單。</p>
+                    </div>
+                    <div class="box">
+                        <p>因不可抗拒之因素或意外天災，導致無法履行預約前來用餐，則無息退還所支付訂金。</p>
+                    </div>
+                </div> 
+                <div class="bookbox bookbox2">
+                    <p class="blue">非常感謝您以寶貴的時間</p>
+                    <p class="blue">詳閱及理解我們的規定</p>
+                    <p class="blue">我們衷心期待與您分享 Le Duet 的美食與服務</p>
+                </div>       
+                <div class="book_btn">
+                    <p class="read blue">我已閱讀並同意接受本服務<br>條款之所有內容</p>
+                    <img class="arrow" src="img/arrowdown.svg">
+                    <a href="">BOOK A TABLE<span>訂位</span></a>
+                </div>
+            </div>
+        </div>
+        <p class="footer f2">Copyright © Le Duet</p> 
+    </div>
+    <div class="content contact" id="contact">
+        <div class="con">
+            <h2 class="fade-in-top2">CONTACT<span>聯絡</span></h2>
+            <div class="conbox fade-in-top4">
+                <h4>Kitchen & Bar</h4>
+                <div class="title">
+                    <h3>Le Duet</h3>
+                    <div class="social">
+                        <a href="https://www.facebook.com/leduet.tw/" target="_blank"><img src="img/icon-fb-b.svg"></a>
+                        <a href="https://www.instagram.com/leduet_tw/" target="_blank"><img src="img/icon-ig-b.svg"></a>
+                    </div>
+                </div>
+                <div class="vol-group">
+                    <div class="vol">
+                        <div class="boxx">
+                            <b>營業時間</b>
+                            <p>週二到週日</p>
+                            <p>18.00 — 00.30</p>
+                        </div>
+                        <div class="boxx">
+                            <b>地址</b>
+                            <p>110 台北市忠孝東路四段</p>
+                            <p>553巷16弄9號</p>
+                        </div>
+                        <div class="boxx">
+                            <b>停車場地址</b>
+                            <p>台北市信義區基隆路一段</p>
+                            <p>176巷1號</p>
+                        </div>
+                        <div class="boxx">
+                            <b>電話</b>
+                            <a href="tel:+886-2-37621570">02 3762 1570</a>
+                        </div>
+                    </div>
+                    <div class="vol">
+                        <div class="boxx">
+                            <b>Opening Times</b>
+                            <p>Tues — Sun</p>
+                            <p>18.00 — 00.30</p>
+                        </div>
+                        <div class="boxx">
+                            <b>Address</b>
+                            <p class="foraddress">No 9, Aly 16, Ln 553, Sect 4 Zhongxiao East Rd, Taipei 110</p>
+                        </div>
+                        <div class="boxx">
+                            <b>Parking Area</b>
+                            <p class="foraddress">No 1, Aly 176,<br>Keelung Rd, Sect 1, <br>Xinyi Dist, Taipei</p>
+                        </div>
+                        <div class="boxx">
+                            <b>Tel</b>
+                            <a href="tel:+886-2-37621570">02 3762 1570</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <iframe class="fade-in-top4" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3614.7403164065604!2d121.56124761500642!3d25.042885383968134!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3442ab4594a73f2f%3A0x4c3400458cc197fd!2sLe%20Duet%20kitchen%20%26%20Bar!5e0!3m2!1szh-TW!2stw!4v1590636525076!5m2!1szh-TW!2stw" width="100%" height="300" frameborder="0" style="border:0;" allowfullscreen=""></iframe>
+        </div>
+        <div class="con blue fade-in-top5">
+            <div class="conbox">
+                <h4 class="partner">Partner store</h4>
+                <div class="title">
+                    <h3>Coquelicot Pâtisserie<span>罌粟甜點</span></h3>
+                    <div class="social">
+                        <a href="https://www.facebook.com/coquelicot2018/" target="_blank"><img src="img/icon-fb.svg"></a>
+                        <a href="https://www.instagram.com/coquelicot_tuan/" target="_blank"><img src="img/icon-ig.svg"></a>
+                    </div>
+                </div>
+                <div class="vol-group">
+                    <div class="vol">
+                        <div class="boxx">
+                            <b>營業時間</b>
+                            <p>週三到週日</p>
+                            <p>13.00 - 19.30</p>
+                            <p>週一二公休</p>
+                        </div>
+                        <div class="boxx">
+                            <b>地址</b>
+                            <p>106 台北市大安區<br>安和路一段127巷5號一樓<br>（信義安和站1號出口，步行約3分鐘）</p>
+                        </div>
+                        <div class="boxx">
+                            <b>電話</b>
+                            <a href="tel:+886-2-3278918">02 2709 3353</a>
+                        </div>
+                    </div>
+                    <div class="vol">
+                        <div class="boxx">
+                            <b>Opening Times</b>
+                            <p>Weds — Sun</p>
+                            <p>13.00 - 19.30</p>
+                            <p>Mon & Tues closed</p>
+                        </div>
+                        <div class="boxx">
+                            <b>Address</b>
+                            <p class="foraddress">No 5, Aly 127, Sect 1,<br>Anhe Rd, Daan Dist,<br>Taipei 106</p>
+                        </div>
+                        <div class="boxx">
+                            <b>Tel</b>
+                            <a href="tel:+886-2-3278918">02 2709 3353</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3614.9796300437542!2d121.5507647503138!3d25.034765344336652!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3442abcea3c0be3d%3A0x2ac1e2bb24d4129c!2zMTA25Y-w5YyX5biC5aSn5a6J5Y2A5a6J5ZKM6Lev5LiA5q61MTI35be3NeiZnw!5e0!3m2!1szh-TW!2stw!4v1583509921282!5m2!1szh-TW!2stw" width="100%" height="300" frameborder="0" style="border:0;" allowfullscreen=""></iframe>
+        </div>
+        <p class="footer f2">Copyright © Le Duet</p> 
+    </div>
+    <script>
+    
+        var swiper = new Swiper('.swiper-container2', {
+          
+          spaceBetween:0,
+          centeredSlides:true,
+          loop:true,
+            grabCursor: true, 
+            cubeEffect: {
+                shadow: false,
+                slideShadows: false,
+                shadowOffset: 0, 
+                shadowScale: 0,
+            },
+          pagination:{
+            el:'.swiper-pagination2',
+            clickable:true,
+          },
+        });
+
+        var swiper = new Swiper('.swiper-container1', {
+          spaceBetween:0,
+          centeredSlides:true,
+          loop:true,
+            grabCursor: true, 
+            cubeEffect: {
+                shadow: false, 
+                slideShadows: false, 
+                shadowOffset: 0, 
+                shadowScale: 0,
+            },
+          pagination:{
+            el:'.swiper-pagination1',
+            clickable:true,
+          },
+        });
+
+    </script>
+</body>
+</html>
